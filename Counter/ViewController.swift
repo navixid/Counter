1bd4d1eb9ae60501f467e057abc2523e1a33a3ee
navix <naviap@yandex.ru>
2024-02-21 10:50:07 +0300

    Проект выполнен

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeHistory: UITextView!
+    
+    private var count = 0
+       
+    @IBOutlet weak var countLabel: UILabel!
+    
+    private var date = Date()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+      
+    
+    @IBAction func tapDownButton(_ sender: UIButton) {
+        if count > 0 {
+            count -= 1
+            countLabel.text = "Значение счётчика: \(count)"
+            changeHistory.text = "\(date.formatRusDate): значение изменено на -1"
+        } else {
+            count = 0
+            changeHistory.text = "\(date.formatRusDate): попытка уменьшить значение счётчика ниже 0"
+        }
+    }
+    
+    
+    @IBAction func tapUpButton(_ sender: UIButton) {
+        count += 1
+        countLabel.text = "Значение счётчика: \(count)"
+        changeHistory.text = "\(date.formatRusDate): значение изменено на +1"
+    }
+    
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        count = 0
+        countLabel.text = "Значение счётчика: \(count)"
+        changeHistory.text = "\(date.formatRusDate): значение сброшено"
+    }
+    
+}
 
-
+extension Date {
+    var formatRusDate: String {
+        let dateFormat = DateFormatter()
+        dateFormat.locale = Locale(identifier: "ru_RU")
+        dateFormat.setLocalizedDateFormatFromTemplate("dd.MM.yyyy HH:mm:ss")
+        return dateFormat.string(from: Date())
+    }
 }
 
