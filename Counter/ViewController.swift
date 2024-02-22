21f4e3a4f690b688ca1d7f2f5f4ee93645051617
navix <naviap@yandex.ru>
2024-02-22 14:22:54 +0300

    Исправил часть ошибок

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,23 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet weak var changeHistory: UITextView!
     
     private var count = 0
        
-    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet private weak var countLabel: UILabel!
     
     private var date = Date()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-      
-    
-    @IBAction func tapDownButton(_ sender: UIButton) {
+    @IBAction private func tapDownButton(_ sender: UIButton) {
         if count > 0 {
             count -= 1
             countLabel.text = "Значение счётчика: \(count)"
@@ -35,14 +29,14 @@ class ViewController: UIViewController {
     }
     
     
-    @IBAction func tapUpButton(_ sender: UIButton) {
+    @IBAction private func tapUpButton(_ sender: UIButton) {
         count += 1
         countLabel.text = "Значение счётчика: \(count)"
         changeHistory.text = "\(date.formatRusDate): значение изменено на +1"
     }
     
     
-    @IBAction func resetButton(_ sender: UIButton) {
+    @IBAction private func resetButton(_ sender: UIButton) {
         count = 0
         countLabel.text = "Значение счётчика: \(count)"
         changeHistory.text = "\(date.formatRusDate): значение сброшено"
